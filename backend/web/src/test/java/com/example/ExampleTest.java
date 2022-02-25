d0a212c7bfef1b5b9b194e7dffff554008b8157e
zhangchunsheng <zhangchunsheng423@gmail.com>
2022-02-25 18:11:12 +0800

    1. add docker

--- backend/web/src/test/java/com/example/ExampleTest.java
+++ backend/web/src/test/java/com/example/ExampleTest.java
@@ -64,7 +64,6 @@ public class ExampleTest {
         } catch(Exception e) {
             e.printStackTrace();
         }
-
     }
 
     @Test
@@ -72,50 +71,21 @@ public class ExampleTest {
         System.out.println("test");
 
         try {
-            InputStream in = null;
-            byte[] cipherData = null;
-
-            in = new FileInputStream(System.getProperty("user.dir") + "/src/test/java/com/example/test.licence");
-            final int len = in.available();
-            cipherData = new byte[len];
-            in.read(cipherData);
-
-            InputStream pubIn;
-            // pubIn = ExampleTest.class.getClassLoader().getResourceAsStream("public.crt");
-            pubIn = new FileInputStream(System.getProperty("user.dir") + "/src/test/java/com/example/public.crt");
-            PublicKey pubKey = getPublicKey(ReadTest.read(pubIn));
-
-            final Cipher cipher = Cipher.getInstance("RSA");
-            cipher.init(2, pubKey);
-
-            byte[] bytes = cipher.doFinal(cipherData);// [B@25b485ba
-            String result = new String(bytes);
-            System.out.println(result);
-            /*
-            * {
-  "code" : "00000000-00000000-00000000-00000000",
-  "organization" : "社区版",
-  "start" : "2020-01-01",
-  "sign" : "63932CC5-E9762317-571074D8-33A8B312",
-  "edition" : "社区版",
-  "expiry" : "2030-01-01"
-}*/
             Map<String, String> resultMap = new HashMap<String, String>();
-            final String json = new String(bytes);
             final ObjectMapper mapper = new ObjectMapper();
             mapper.configure(SerializationFeature.INDENT_OUTPUT, true);
 
             resultMap.put("code", "00000000-00000000-00000000-00000000");
-            resultMap.put("organization", "社区版");
+            resultMap.put("organization", "商业版");
             resultMap.put("start", "2020-01-01");
-            resultMap.put("edition", "社区版");
+            resultMap.put("edition", "商业版");
             resultMap.put("expiry", "2030-01-01");
 
-            System.out.println(c(resultMap));// 63932CC5-E9762317-571074D8-33A8B312
+            String sign = c(resultMap);// 8CAE88E0-F70DC710-2988BA2E-B07CE5C5
+            System.out.println(sign);
         } catch(Exception e) {
             e.printStackTrace();
         }
-
     }
 
     private static String c(final Map<String, String> map) {
